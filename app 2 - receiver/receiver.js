677153ea9cd0dafcbfef10efc0c589aca7fdb521
[Piotr Pastuszek] <[piotr.pastuszek@bluesoft.com]>
2022-07-21 00:12:31 +0200

    Add app 2 to repository (+ gitignore)

---
+++ app 2 - receiver/receiver.js
@@ -0,0 +1,26 @@
+const amqp = require('amqplib/callback_api');
+
+var CONFIG = require('./config.json');
+var HOST = CONFIG.HOST;
+var QUEUE = CONFIG.QUEUE;
+
+amqp.connect(HOST, (connError, connection) => {
+    if (connError) {
+        throw connError;
+    }
+
+    connection.createChannel((channelError, channel) => {
+        if (channelError) {
+            throw channelError;
+        }
+
+        channel.assertQueue(QUEUE);
+
+        channel.consume(QUEUE, (msg) => {
+            console.log(msg.content.toString())
+        }, {
+            noAck: true
+        })
+
+    })
+})
