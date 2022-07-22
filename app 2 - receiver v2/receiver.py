eb24eceddb5eb54568c3886834d1278a57ffcedc
[Piotr Pastuszek] <[piotr.pastuszek@bluesoft.com]>
2022-07-22 16:29:43 +0200

    app 2 updated - connection to REDIS

--- app 2 - receiver v2/receiver.py
+++ app 2 - receiver v2/receiver.py
@@ -4,6 +4,8 @@ import sys
 import os
 import yaml
 import json
+import redis
+from redis.commands.json.path import Path
 from yaml.loader import SafeLoader
 from multiprocessing import connection
 
@@ -12,6 +14,9 @@ with open('config.yml') as configuration:
 
 HOST = data['HOST']
 QUEUE = data['QUEUE']
+
+REDIS_HOST = data['REDIS_HOST']
+REDIS_PORT = data['REDIS_PORT']
 
 def main():
     connection = pika.BlockingConnection(
@@ -22,13 +27,26 @@ def main():
 
     def callback(ch, method, properties, body):
         print(body)
-        json_object = json.loads(body)
-        print(json_object)
+        object = json.loads(body)
+        print(object)
+
+        def pass_to_redis():
+            try:
+                r = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
+                r.set('msg_from_rabbitmq', json.dumps(object))
+                r.expire('msg_from_rabbitmq', 60)
+            except Exception as e:
+                print(e)
+
+        if __name__ == '__main__':
+            pass_to_redis()
 
     channel.basic_consume(queue=QUEUE, on_message_callback=callback, auto_ack=True)
 
     print('Listening')
     channel.start_consuming()
+
+
 
 if __name__ == '__main__':
     try:
