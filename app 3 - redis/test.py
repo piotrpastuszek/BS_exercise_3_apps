f3e190f83e645a417e5a88afb9a85cd602add730
[Piotr Pastuszek] <[piotr.pastuszek@bluesoft.com]>
2022-07-22 12:19:03 +0200

    import data from yml file to app 3

--- app 3 - redis/test.py
+++ app 3 - redis/test.py
@@ -1,9 +1,17 @@
 from itertools import count
 import redis
+import yaml
+import yaml.loader
+
+with open('config.yml') as configuration:
+    data = yaml.load(configuration, Loader=yaml.FullLoader)
+
+HOST = data['HOST']
+PORT = data['PORT']
 
 r = redis.Redis(
-    host='localhost',
-    port=6379)
+    host=HOST,
+    port=PORT)
 
 count = redis.Redis()
 size = count.dbsize()
